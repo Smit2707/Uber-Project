cfc59d200a0ead31bc20d9bb60903cc273ddca35
Smit Yadav <Smityadav2707@gmail.com>
2025-09-09 23:37:18 +0530

    home screen ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,80 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { userDataContext } from '../context/UserContext'
+import { useGSAP } from "@gsap/react"
+import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel';
 
 const Home = () => {
   const data = useContext(userDataContext);
-  console.log(data);
+  // console.log(data);
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: "70%"
+      })
+    }
+    else {
+      gsap.to(panelRef.current, {
+        height: "0%"
+      })
+    }
+    gsap.to(panelRef.current, {
+      ease: "elastic.inOut",
+      duration: 2
+    })
+  }, [panelOpen]);
+
   return (
-    <div>Home</div>
+    <div className='relative h-screen'>
+      <h3 className='text-3xl absolute text-white pt-[6%] pl-[6%] font-[500] tracking-wider'>Uber</h3>
+      <div className='h-screen w-screen'>
+        <img className='h-full w-full object-cover' src="/uber_map2.png" alt="" />
+      </div>
+      <div className='absolute flex flex-col justify-end top-0 h-screen w-full'>
+        <div className='h-[30%] p-6 bg-white'>
+          {panelOpen ?
+            <h5 onClick={() => setPanelOpen(false)} className='absolute top-6 right-5 text-xl px-2 font-bold text-white bg-black rounded-full'>
+              <i className="ri-arrow-down-wide-fill"></i>
+            </h5>
+            :
+            <h5 onClick={() => setPanelOpen(true)} className='absolute top-6 right-5 text-xl px-2 font-bold bg-white rounded-full'>
+              <i className="ri-arrow-up-wide-line"></i>
+            </h5>}
+
+          <h4 className='text-2xl font-semibold'>Find A Trip</h4>
+          <form onSubmit={(e) => { submitHandler(e) }}>
+            <input
+              value={pickup}
+              onClick={() => setPanelOpen(true)}
+              onChange={(e) => setPickup(e.target.value)}
+              className='bg-[#eee] outline-none px-10 py-2 rounded-lg text-sm w-full mt-5 mb-2'
+              type="text"
+              placeholder='Add Your Pick-Up Location'
+            />
+            <input
+              value={destination}
+              onClick={() => setPanelOpen(true)}
+              onChange={(e) => setDestination(e.target.value)}
+              className='bg-[#eee] outline-none px-10 py-2 rounded-lg text-sm w-full'
+              type="text"
+              placeholder='Add Your Destination Name'
+            />
+          </form>
+        </div>
+        <div ref={panelRef} className='h-[0%] bg-green-600'>
+          <LocationSearchPanel />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/StartScreen.jsx
+++ frontend/src/pages/StartScreen.jsx
@@ -5,9 +5,9 @@ import { FaArrowRightLong } from "react-icons/fa6";
 const StartScreen = () => {
   return (
     <div className='h-screen w-full'>
-      <div className='bg-[url("https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] bg-cover bg-center h-[80%] w-full'>
+      <div className='bg-[url("https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=1344/height=896/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy9hM2NmODU2NC1lMmE2LTQxOGMtYjliMC02NWRkMjg1YzEwMGIuanBn")] bg-cover bg-center h-[80%] w-full'>
         {/* <img src="https://imgs.search.brave.com/PFUVRSLMkJrpRUDx0GRSiqhB8nUIr7GKfiwHneacgxk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2l0eXBuZy5jb20v/cHVibGljL3VwbG9h/ZHMvcHJldmlldy91/YmVyLXRheGktd2hp/dGUtbG9nby1oZC1w/bmctNzAxNzUxNjk0/NzA3MjEzYXJzYWpn/bmVidS5wbmc" className='h-20' alt="" /> */}
-        <h3 className='text-3xl text-white pt-[6%] pl-[6%] font-[500] tracking-wider'>Uber</h3>
+        <h3 className='text-3xl text-black pt-[6%] pl-[6%] font-[600] tracking-wider'>Uber</h3>
       </div>
       <div className='flex flex-col flex-1 h-[20%] text-black px-4 pt-4'>
         <h2 className='text-2xl font-semibold'>Get Started With Uber</h2>
